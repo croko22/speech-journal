cfcc71580395f30c997ef0a017e69555fbb25886
Kevin Chambi <kchambit@unsa.edu.pe>
2023-02-14 11:47:35 -0500

    Fixing the JSession function hell

--- src/pages/InSession.jsx
+++ src/pages/InSession.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { v4 } from "uuid";
 import GrabarNota from "../components/GrabarNota";
 import "./InSession.scss";
 
 const InSession = () => {
+  const navigate = useNavigate();
+  //? Get stored sessions
   const storedSessions = JSON.parse(localStorage.getItem("Sessions"));
   const [index, setIndex] = useState(0);
   //? Question phases (Think, answer, next)
@@ -30,6 +33,7 @@ const InSession = () => {
     localStorage.setItem("Logs", JSON.stringify(tmpLogs));
   };
 
+  //TODO: Arreglar el coso de que muestre 2 veces la primera pregunta y no muestre la ultima
   //? Timer
   useEffect(() => {
     counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
@@ -41,6 +45,7 @@ const InSession = () => {
     if (counter === 0 && questionsPhase === "answer") {
       setQuestionsPhase(quesTionPhases[2]);
       setCounter(5);
+      setIsListening(false);
     }
     if (
       counter === 0 &&
@@ -52,15 +57,28 @@ const InSession = () => {
       //? Change question
       setIndex(index + 1);
       setCurrentQuestion(storedSessions[index]);
-      setQuestionsPhase(quesTionPhases[0]);
-      setCounter(currentQuestion.timeToThink);
-      setIsListening(false);
+      setCounter(storedSessions[index].timeToThink);
       //? Reset note
-      setNote({ question: currentQuestion.question, text: "" });
-    } else if (counter === 0 && questionsPhase === "next") {
+      setNote({
+        question: storedSessions[index].question,
+        text: "",
+      });
+      setQuestionsPhase(quesTionPhases[0]);
+    }
+    if (
+      counter === 0 &&
+      questionsPhase === "next" &&
+      index === storedSessions.length - 1
+    ) {
+      // setAllNotes([...allNotes, note]); //? Save note
+      // handleSaveNote();
       setQuestionsPhase(quesTionPhases[3]);
-      setIsListening(false);
+      setCounter(3);
+    }
+    if (counter === 0 && questionsPhase === "end") {
+      setAllNotes([...allNotes, note]); //? Save note
       handleSaveNote();
+      navigate("/saved-logs");
     }
   }, [counter]);
 
@@ -70,7 +88,13 @@ const InSession = () => {
         {questionsPhase[0].toUpperCase() + questionsPhase.slice(1)}
       </h1>
       <p className="timer">{counter}</p>
-      {<h1>{currentQuestion.question}</h1>}
+      {
+        <h1>
+          {questionsPhase !== "next" &&
+            questionsPhase !== "end" &&
+            currentQuestion.question}
+        </h1>
+      }
       {questionsPhase === "think"
         ? "Think and reflect for some time..."
         : questionsPhase === "next"
--- src/pages/SavedLogs.jsx
+++ src/pages/SavedLogs.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const SavedLogs = () => {
-  return <div>SavedLogs</div>;
+  const savedLogs = JSON.parse(localStorage.getItem("Logs")) || [];
+  return (
+    <div>
+      <h1>Saved Logs</h1>
+      <ul>
+        {savedLogs.map((log, index) => (
+          <li key={index}>
+            <p>{log.date}</p>
+            {log.QAs.map((qa, index) => (
+              <div key={index}>
+                <p>{qa.question}</p>
+                <p>{qa.text}</p>
+              </div>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default SavedLogs;
