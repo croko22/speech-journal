a8f19db594a8ac3e94cdc1a13edb14952d6fbb3a
Kevin Chambi <kchambit@unsa.edu.pe>
2023-02-25 11:38:47 -0500

    Home section (Start a new section and Recent logs)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,71 +1,41 @@
 import React, { useState, useEffect } from "react";
+import { FaPlay } from "react-icons/fa";
 import { v4 } from "uuid";
 import "./Home.scss";
-import GrabarNota from "../components/GrabarNota";
-import Notas from "../components/Notas";
 
 //*Estructura de Nota
 const nota = { title: "", text: "" };
 function Home() {
-  const [savedNotes, setSavedNotes] = useState(
-    JSON.parse(localStorage.getItem("notas")) || []
+  const [savedLogs, setSavedLogs] = useState(
+    JSON.parse(localStorage.getItem("Logs")) || []
   );
-  const [note, setNote] = useState(nota);
-  const [isEditing, setIsEditing] = useState(false);
-  //?Handle speech input
-  const [isListening, setIsListening] = useState(false);
-
-  useEffect(() => {
-    localStorage.setItem("notas", JSON.stringify(savedNotes));
-  }, [savedNotes]);
-
-  // Gestion de las notas
-  const handleSaveNote = () => {
-    if (isEditing) {
-      const newNotes = savedNotes.filter((nota) => nota.id !== note.id);
-      const tmpNotas = [...newNotes, note];
-      setSavedNotes(tmpNotas);
-      setIsEditing(false);
-    } else {
-      const date = new Date();
-      let tmpNota = { ...note, id: v4(), date: date.toLocaleDateString() };
-      const tmpNotas = [...savedNotes, tmpNota];
-      setSavedNotes(tmpNotas);
-    }
-    setNote(nota);
-  };
-
-  const deleteNote = (id) => {
-    const newNotes = savedNotes.filter((note) => note.id !== id);
-    setSavedNotes(newNotes);
-  };
-
-  const editNote = (id) => {
-    const date = new Date();
-    const noteToEdit = savedNotes.filter((note) => note.id == id)[0];
-    setNote({
-      title: noteToEdit.title,
-      text: noteToEdit.text,
-      id: noteToEdit.id,
-      date: date.toLocaleDateString(),
-    });
-    setIsEditing(true);
-  };
 
   return (
     <div className="container">
-      {/* <GrabarNota
-        note={note}
-        setNote={setNote}
-        handleSaveNote={handleSaveNote}
-        isListening={isListening}
-        setIsListening={setIsListening}
-      /> */}
-      <Notas
-        savedNotes={savedNotes}
-        handleDeleteNote={deleteNote}
-        handleEditNote={editNote}
-      />
+      {/*//?Start a session*/}
+      <div className="box start-session-container">
+        <h1>Start a new session</h1>
+        <a href="/in-session" className="start-session">
+          <FaPlay className="play-icon" />
+        </a>
+      </div>
+      {/*//?Recent logs*/}
+      <div className="box recent-logs-container">
+        <h1>Recent logs</h1>
+        <div className="logs">
+          {savedLogs.reverse().map((log, index) => (
+            <div className="log" key={index}>
+              <p className="date">{log.date}</p>
+              {log.QAs.map((qa, index) => (
+                <div key={index}>
+                  <h5>{qa.question}</h5>
+                  <p>{qa.text}</p>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
