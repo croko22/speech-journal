a0be048b0db1fa515790a14b4de19f45dd8deb10
Kevin Chambi <kchambit@unsa.edu.pe>
2023-02-25 11:40:54 -0500

    Log session update, DeleteLog

--- src/pages/SavedLogs.jsx
+++ src/pages/SavedLogs.jsx
@@ -1,20 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaTrashAlt, FaRegEdit } from "react-icons/fa";
 import "./SavedLogs.scss";
 
 const SavedLogs = () => {
-  const savedLogs = JSON.parse(localStorage.getItem("Logs")) || [];
+  const [savedLogs, setSavedLogs] = useState(
+    JSON.parse(localStorage.getItem("Logs")) || []
+  );
 
   const deleteLog = (id) => {
     const tmpLogs = savedLogs.filter((log) => log.id !== id);
-    localStorage.setItem("Logs", JSON.stringify(tmpLogs));
+    setSavedLogs(tmpLogs);
   };
+
+  useEffect(() => {
+    localStorage.setItem("Logs", JSON.stringify(savedLogs));
+  }, [savedLogs]);
 
   return (
     <div>
       <ul className="logs-containter">
         {savedLogs.map((log, index) => (
           <li className="log-card" key={index}>
-            <p className="date">{log.date}</p>
+            <p className="date">
+              {log.date}
+              <FaTrashAlt onClick={() => deleteLog(log.id)} />
+            </p>
             {log.QAs.map((qa, index) => (
               <div key={index}>
                 <h5>{qa.question}</h5>
