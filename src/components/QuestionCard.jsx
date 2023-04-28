30af55b61d489ba0dc1d42ab0232532af1931c04
kchambit <kev132011@hotmail.com>
2023-04-28 11:57:24 -0500

    Refactoring and styling for question card

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -6,67 +6,61 @@ const SessionCard = ({
   sQuestion,
   deleteQuestion,
   addQuestion,
-  editQuestion,
+  updateQuestion,
   addMode,
   setAddMode,
 }) => {
   const [question, setQuestion] = useState(sQuestion);
   return (
-    <>
-      <div className="questionCard">
-        {addMode ? (
-          <div className="btnsBox">
-            <button className="btn-note">
-              <FaTimes onClick={() => setAddMode(false)} />
-            </button>
-            <button className="btn-note" onClick={() => addQuestion(question)}>
-              <FaRegSave />
-            </button>
-          </div>
-        ) : (
-          <div>
-            <button
-              className="btn-note"
-              onClick={() => deleteQuestion(question.id)}
-            >
-              <FaTrashAlt />
-            </button>
-            <button className="btn-note" onClick={() => editQuestion(question)}>
-              <FaRegEdit />
-            </button>
-          </div>
-        )}
-        <h3>{sQuestion.question}</h3>
-        <input
-          onChange={(e) =>
-            setQuestion({ ...question, question: e.target.value })
-          }
-          className="questionInput"
-          type="text"
-          placeholder="Question?"
-        />
-        <div className="timeSelectSection">
-          <label>Time to think: </label>
-          <input
-            onChange={(e) =>
-              setQuestion({ ...question, timeToThink: e.target.value })
-            }
-            className="timeSelectInput"
-            type="number"
-            defaultValue={sQuestion.timeToThink}
-          />
-          <label>Time to answer: </label>
-          <input
-            onChange={(e) =>
-              setQuestion({ ...question, timeToAnswer: e.target.value })
-            }
-            className="timeSelectInput"
-            type="number"
-            defaultValue={sQuestion.timeToAnswer}
+    <div className="questionCard">
+      {addMode ? (
+        <div>
+          <FaTimes className="btn-note" onClick={() => setAddMode(false)} />
+          <FaRegSave
+            className="btn-note"
+            onClick={() => addQuestion(question)}
           />
         </div>
+      ) : (
+        <div>
+          <FaTrashAlt
+            className="btn-note"
+            onClick={() => deleteQuestion(question.id)}
+          />
+          <FaRegEdit
+            className="btn-note"
+            onClick={() => updateQuestion(question.id, question)}
+          />
+        </div>
+      )}
+      <input
+        value={question.question}
+        onChange={(e) => setQuestion({ ...question, question: e.target.value })}
+        className="questionInput"
+        type="text"
+        placeholder="Question?"
+      />
+      <div className="timeSelectSection">
+        <label className="timeSelectLabel">Time to think: </label>
+        <input
+          onChange={(e) =>
+            setQuestion({ ...question, timeToThink: e.target.value })
+          }
+          className="timeSelectInput"
+          type="number"
+          defaultValue={sQuestion.timeToThink}
+        />
+        <label className="timeSelectLabel">Time to answer: </label>
+        <input
+          onChange={(e) =>
+            setQuestion({ ...question, timeToAnswer: e.target.value })
+          }
+          className="timeSelectInput"
+          type="number"
+          defaultValue={sQuestion.timeToAnswer}
+        />
       </div>
-    </>
+    </div>
   );
 };
 
