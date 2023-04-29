721e428cec0b0722400bcbcd728fd108e559b6c4
kchambit <kev132011@hotmail.com>
2023-04-29 16:45:45 -0500

    c: Question card optimistic updates

--- src/components/QuestionCard.jsx
+++ src/components/QuestionCard.jsx
@@ -1,35 +1,108 @@
 import React, { useState } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { FaTrashAlt, FaRegEdit, FaTimes, FaRegSave } from "react-icons/fa";
+import axios from "axios";
+import { v4 } from "uuid";
+
 import "./QuestionCard.scss";
 
 const SessionCard = ({
-  sQuestion,
-  deleteQuestion,
-  addQuestion,
-  updateQuestion,
-  addMode,
-  setAddMode,
+  sQuestion = {
+    id: v4(),
+    question: "",
+    timeToThink: 15,
+    timeToAnswer: 15,
+  },
+  activeSession,
+  setActiveSession,
+  addQuestionMode,
+  setAddQuestionMode,
 }) => {
+  const queryClient = useQueryClient();
   const [question, setQuestion] = useState(sQuestion);
+
+  const patchSession = async (sessionData) => {
+    axios.patch(
+      `http://localhost:3000/journal-sessions/${activeSession._id}`,
+      sessionData
+    );
+  };
+
+  const addQuestionMutation = useMutation({
+    mutationFn: () => {
+      patchSession({ questions: [...activeSession.questions, question] });
+    },
+    onMutate: () => {
+      const tmpSession = {
+        ...activeSession,
+        questions: [...activeSession.questions, question],
+      };
+      setActiveSession(tmpSession);
+      setAddQuestionMode(false);
+      return () => setActiveSession(activeSession);
+    },
+    onError: (err, id, rollback) => rollback(),
+    onSettled: () => queryClient.invalidateQueries("savedSessions"),
+  });
+
+  const deleteQuestionMutation = useMutation({
+    mutationFn: () =>
+      patchSession({
+        questions: activeSession.questions.filter((q) => q.id !== question.id),
+      }),
+    onMutate: () => {
+      const updatedQuestions = activeSession?.questions.filter(
+        (q) => q.id !== question.id
+      );
+      const tmpSession = {
+        ...activeSession,
+        questions: updatedQuestions,
+      };
+      setActiveSession(tmpSession);
+      return () => setActiveSession(activeSession);
+    },
+    onError: (err, id, rollback) => rollback(),
+    onSettled: () => queryClient.invalidateQueries("savedSessions"),
+  });
+
+  const updateQuestionMutation = useMutation({
+    mutationFn: () => patchSession({ questions: activeSession.questions }),
+    onMutate: () => {
+      const updatedQuestions = activeSession?.questions.map((q) =>
+        q.id === question.id ? question : q
+      );
+      const tmpSession = {
+        ...activeSession,
+        questions: updatedQuestions,
+      };
+      setActiveSession(tmpSession);
+      return () => setActiveSession(activeSession);
+    },
+    onError: (err, id, rollback) => rollback(),
+    onSettled: () => queryClient.invalidateQueries("savedSessions"),
+  });
   return (
     <div className="questionCard">
-      {addMode ? (
+      {addQuestionMode ? (
         <div>
-          <FaTimes className="btn-note" onClick={() => setAddMode(false)} />
+          <FaTimes
+            className="btn-note"
+            onClick={() => setAddQuestionMode(false)}
+          />
           <FaRegSave
             className="btn-note"
-            onClick={() => addQuestion(question)}
+            onClick={() => addQuestionMutation.mutate()}
           />
         </div>
       ) : (
         <div>
           <FaTrashAlt
             className="btn-note"
-            onClick={() => deleteQuestion(question.id)}
+            onClick={() => deleteQuestionMutation.mutate()}
           />
           <FaRegEdit
             className="btn-note"
-            onClick={() => updateQuestion(question.id, question)}
+            onClick={() => updateQuestionMutation.mutate()}
           />
         </div>
       )}
