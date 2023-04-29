bc71005b44c26ec5f1f3982d469ba1c7a2cbb317
kchambit <kev132011@hotmail.com>
2023-04-29 16:50:16 -0500

    f:ActiveSession component, handling state of the sessions

---
+++ src/components/ActiveSession/ActiveSession.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from "react";
+import { FaPlus, FaRegSave } from "react-icons/fa";
+import QuestionCard from "../QuestionCard";
+
+const ActiveSession = ({
+  activeSession,
+  setActiveSession,
+  savedSessions,
+  setSavedSessions,
+}) => {
+  const [addQuestionMode, setAddQuestionMode] = useState(false);
+
+  useEffect(() => {
+    setSavedSessions(
+      savedSessions.map((s) =>
+        s._id === activeSession._id ? activeSession : s
+      )
+    );
+  }, [activeSession]);
+
+  return (
+    <div className="questions-container box">
+      {/* //*Rendered QuestionCards*/}
+      <div className="qc-header">
+        <h1>{activeSession.name}</h1>
+        <FaRegSave />
+      </div>
+      {activeSession?.questions?.map((question) => (
+        <QuestionCard
+          key={question.id}
+          sQuestion={question}
+          activeSession={activeSession}
+          setActiveSession={setActiveSession}
+        />
+      ))}
+      {addQuestionMode ? (
+        <QuestionCard
+          activeSession={activeSession}
+          setActiveSession={setActiveSession}
+          addQuestionMode={addQuestionMode}
+          setAddQuestionMode={setAddQuestionMode}
+        />
+      ) : (
+        <button
+          className="newQuestionBtn"
+          onClick={() => setAddQuestionMode(true)}
+        >
+          <FaPlus /> Add new question
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ActiveSession;
