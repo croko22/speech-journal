08cf7996e712fb526e02a226de9887627d708cea
kchambit <kev132011@hotmail.com>
2023-05-02 11:38:00 -0500

    c:Header improvements

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,52 +8,52 @@ const Header = () => {
       <a className="header-title" href="/">
         🎤 Speech Journal
       </a>
-      {window.localStorage.getItem("authData") ? (
-        <nav>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "active-header-link" : "header-link"
-            }
-            to="/session"
-          >
-            <FaCog className="icon" />
-            <span>Journal Session</span>
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "active-header-link" : "header-link"
-            }
-            to="/saved-logs"
-          >
-            <FaFileAlt className="icon" />
-            <span>Saved logs</span>
-          </NavLink>
-          <NavLink
-            className={({ isActive }) =>
-              isActive ? "active-header-link" : "header-link"
-            }
-            to="/in-session"
-          >
-            <FaPlay className="icon" />
-            <span>Start session</span>
-          </NavLink>
-          <button
-            className="logout"
-            onClick={() => {
-              window.localStorage.removeItem("authData");
-              window.location.reload();
-            }}
-          >
-            Logout
-          </button>
-        </nav>
-      ) : (
-        <nav>
+      <nav>
+        {window.localStorage.getItem("authData") ? (
+          <>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "active-header-link" : "header-link"
+              }
+              to="/session"
+            >
+              <FaCog className="icon" />
+              <span>Journal Session</span>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "active-header-link" : "header-link"
+              }
+              to="/saved-logs"
+            >
+              <FaFileAlt className="icon" />
+              <span>Saved logs</span>
+            </NavLink>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "active-header-link" : "header-link"
+              }
+              to="/in-session"
+            >
+              <FaPlay className="icon" />
+              <span>Start session</span>
+            </NavLink>
+            <button
+              className="logout"
+              onClick={() => {
+                window.localStorage.removeItem("authData");
+                window.location.reload();
+              }}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
           <a className="login" href="/auth">
             Login
           </a>
-        </nav>
-      )}
+        )}
+      </nav>
     </header>
   );
 };
