74b97f4449055926fc724e6431d73c65cca2b36c
kchambit <kev132011@hotmail.com>
2023-05-10 12:36:57 -0500

    Landing improvement, still sucks...

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useStore } from "../hooks/useStore";
-
 import "./Landing.scss";
 
 const Landing = () => {
@@ -14,17 +13,27 @@ const Landing = () => {
 
   return (
     <div className="landing-container">
-      <h1>🎤 Speech journal 🎤</h1>
-      <p className="hero-text">
-        Speech journal is a tool to help you improve your speech and
-        communication skills while journaling.
-      </p>
-      <p>
-        It allows you to record your speech and then analyze it to find out what
-        you can improve.
-      </p>
-      <p>It also allows you to save your sessions and review them later.</p>
-      <img className="hero-img" src="/speech-journal-hero.png" alt="hero" />
+      <div className="hero">
+        <div className="">
+          <h1>🎤 Speech journal 🎤</h1>
+          <p className="hero-text">
+            Speech journal is a tool to help you improve your speech and
+            communication skills while journaling.
+          </p>
+          <p>
+            It allows you to record your speech and then analyze it to find out
+            what you can improve.
+          </p>
+          <small>
+            It also allows you to save your sessions and review them later.
+          </small>
+          <br />
+          <a className="login" href="/auth">
+            Join now
+          </a>
+        </div>
+        <img className="hero-img" src="/speech-journal-hero.png" alt="hero" />
+      </div>
     </div>
   );
 };
