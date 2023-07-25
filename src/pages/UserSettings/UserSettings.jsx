6c62337f21daff7867d79c99d15362ed80a1ba5c
croko22 <kev132011@hotmail.com>
2023-07-25 10:33:54 -0500

    c: Applied BEM in UserSettings page

--- src/pages/UserSettings/UserSettings.jsx
+++ src/pages/UserSettings/UserSettings.jsx
@@ -15,42 +15,44 @@ const UserSettings = () => {
   };
 
   return (
-    <div className="user-settings-page">
-      <h1 className="page-title">Account</h1>
-      <p className="username">Username: {user.username}</p>
-      <p className="email">Email: {user.email}</p>
-      <h2>
-        Danger Zone <FaSkull />
-      </h2>
-      <small>
-        "Are you sure you want to delete your account? This action cannot be
-        undone."
-      </small>
-      <button
-        className="delete-button"
-        onClick={() => setShowDeleteButton(true)}
-      >
-        Delete Account
-      </button>
-      {showDeleteButton && (
-        <div className="delete-confirm">
-          <p>Are you sure you want to delete your account?</p>
-          <div className="delete-confirm__buttons">
-            <button
-              className="delete-confirm__buttons-button"
-              onClick={handleDeleteClick}
-            >
-              <FaCheck /> Yes
-            </button>
-            <button
-              className="delete-confirm__buttons-button"
-              onClick={() => setShowDeleteButton(false)}
-            >
-              <FaTimes /> No
-            </button>
+    <div className="user-settings">
+      <h1 className="user-settings__title">Account</h1>
+      <div className="user-settings__data">
+        <h2>User data</h2>
+        <p className="user-settings__data__username">
+          Username: {user.username}
+        </p>
+        <p className="user-settings__data__email">Email: {user.email}</p>
+      </div>
+      <hr />
+      <div className="user-settings__danger-zone">
+        <h2 className="user-settings__danger-zone__title">
+          Danger Zone <FaSkull />
+        </h2>
+        <small className="user-settings__danger-zone__message">
+          "Are you sure you want to delete your account? This action cannot be
+          undone."
+        </small>
+        <button
+          className="user-settings__danger-zone__delete-button"
+          onClick={() => setShowDeleteButton(true)}
+        >
+          Delete Account
+        </button>
+        {showDeleteButton && (
+          <div className="user-settings__danger-zone__delete-confirm">
+            <p>Are you sure you want to delete your account?</p>
+            <div className="user-settings__danger-zone__delete-confirm__buttons">
+              <button onClick={handleDeleteClick}>
+                <FaCheck /> Yes
+              </button>
+              <button onClick={() => setShowDeleteButton(false)}>
+                <FaTimes /> No
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
