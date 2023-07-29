cd2918ea7015ed8cbbf074e11b508a78a1cda36c
croko22 <kev132011@hotmail.com>
2023-07-29 17:44:53 -0500

    c: Replaced useStorage for storage hook

--- src/hooks/storage.js
+++ src/hooks/storage.js
@@ -2,7 +2,7 @@ const storagePrefix = "speech_journal_";
 //TODO: Change token name to something more generic
 const storage = {
   getToken: () => {
-    return JSON.parse(window.localStorage.getItem(`${storagePrefix}authData`));
+    return JSON.parse(window.localStorage.getItem(`${storagePrefix}token`));
   },
   setToken: (token) => {
     window.localStorage.setItem(`${storagePrefix}token`, JSON.stringify(token));
